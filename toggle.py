da97f82eb8364c417f90bfac1c7304554f4c87cf
krikx <vamsi_ism@outlook.com>
2015-05-10 16:24:00 +0530

    Added quit command.

--- toggle.py
+++ toggle.py
@@ -1,11 +1,18 @@
 import sublime, sublime_plugin
 
-class CustomFullScreen(sublime_plugin.WindowCommand):
-	def run(self):
-		self.window.run_command('toggle_full_screen')
-		self.window.run_command('toggle_menu')
+class CustomFullScreenCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        self.window.run_command('toggle_full_screen')
+        self.window.run_command('toggle_menu')
+
+class QuitCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        self.window.run_command('toggle_full_screen')
+        self.window.run_command('toggle_menu')
+        self.window.run_command('close_window')
+
 
 def plugin_loaded():
-	window = sublime.active_window()
-	window.run_command('toggle_full_screen')
-	window.run_command('toggle_menu')
+    window = sublime.active_window()
+    window.run_command('toggle_full_screen')
+    window.run_command('toggle_menu')
