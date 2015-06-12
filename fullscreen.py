4b214772de07cc5159599ffb96f79e6fff4c8866
krikx <vamsi_ism@outlook.com>
2015-06-12 13:20:08 +0530

    Bug fix.

--- fullscreen.py
+++ fullscreen.py
@@ -1,5 +1,7 @@
 import sublime, sublime_plugin
 
+auto_toggle = False
+
 class CustomFullScreenCommand(sublime_plugin.WindowCommand):
     def run(self):
         self.window.run_command('toggle_full_screen')
@@ -7,14 +9,17 @@ class CustomFullScreenCommand(sublime_plugin.WindowCommand):
 
 class QuitCommand(sublime_plugin.WindowCommand):
     def run(self):
-        self.window.run_command('toggle_full_screen')
-        self.window.run_command('toggle_menu')
+        if auto_toggle:
+            self.window.run_command('toggle_full_screen')
+            self.window.run_command('toggle_menu')
         self.window.run_command('close_window')
 
 
 def plugin_loaded():
+    global auto_toggle
     settings = sublime.load_settings('Fullscreen.sublime-settings')
-    if settings.get('fullscreen_on_start', False):
+    auto_toggle = settings.get('fullscreen_on_start', False)
+    if auto_toggle:
         window = sublime.active_window()
         window.run_command('toggle_full_screen')
         window.run_command('toggle_menu')
